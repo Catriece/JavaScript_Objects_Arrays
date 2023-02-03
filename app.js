eb940e0a927caf178f69ae5ae0ee7e650866fc89
Catriece Gilbert <catriece.gilbert@gmail.com>
2023-02-03 14:46:32 -0500

    Objects and Arrays Exercise

--- app.js
+++ app.js
@@ -2,93 +2,99 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
-/* 
-// Reduce Method
 
-const numbers = [2, 22, 12, 17, 18, 39, 129]
-const Sum = numbers.reduce((p, c) => {
-    console.log(`Previous Value: ${p}`)
-    console.log(`Current Value: ${c}`)
-        return p + c;
-}, 0);
+const numbers = [2, 22, 12, 17, 18, 39, 129];
+let sum = 0;
 
-    console.log(Sum);
-
-// forEach Method
-function arraySum(numbers) {
-    let sum = 0
+function arraySum() {
     numbers.forEach((number) => {
         sum += number;
-    });
+    })
     return sum
-}
-
-console.log(arraySum(numbers))
+    }
+console.log(arraySum(numbers));
 
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
 
-let book = {};
+let book = {}
 
 book.title = "The Wild Robot";
 book.author = "Peter Brown";
-book.pages = 230;
+book.pageCount = 230;
 book.readCount = 2;
 
-console.log(book)
-
-book.info = function () {
-    return `${book.title} by ${book.author}, ${book.pages} pages, read ${book.readCount} times.`
+book.info = function() {
+    return `${this.title} by ${this.author}, ${this.pageCount} pages, read ${this.readCount} times.`
 }
 
-console.log(book.info())
+console.log(book.info());
 
-
-// Exercise 3 Section
+// Exercise 3 Section - Followed The Video
 console.log("EXERCISE 3:\n==========\n");
 
 let sentence = "The quick brown fox jumps over the lazy dog";
-const revSentence = sentence.split(" ")
-
-let answer = []
-
-for (let i = 0; i < revSentence.length; i++) {
-   answer.push(revSentence[i].split("").reverse().join(""))
+function reverseWords(sentence) {
+    let result = []
+    let newSentence = sentence.split(" ")
+    for (let i = 0; i < newSentence.length; i++) {
+        let letters = newSentence[i].split("")
+        let newWord = letters.reverse()
+        let reverseWord = letters.join("")
+        result.push(reverseWord);
+    }
+    return result.join(" ");
 }
 
-let joinAnswer = answer.join(" ")
 
-console.log(joinAnswer) */
-
+console.log(reverseWords(sentence));
 
 // Exercise 4 Section
 console.log("EXERCISE 4:\n==========\n");
 
-/*
+// My own attempt
 
-6. And create an object for each row (or table record)
-7. Assign the values from the parsed row values to `name` and `age` values on the object
-8. Add each new object to a result array (should be defined before the loop block */
+/*
+let csvData = "name,age\nFrodo,50\nSam,38\nMerry,36\nPippin,26";
+let newData = csvData.split("\n");
+let dataHeaders = newData[0].split(",")
+
+let result = []
+
+for (let i = 1; i < newData.length; i++) {
+    let outcome = newData[i].split(",");
+    //Object.fromEntries(outcome)
+ 
+    console.log(outcome)
+}
+//let obj = Object.fromEntries(outcome) */
+
+//console.log(outcome)
+
+// Video Follow Along
 
 let csvData = "name,age\nFrodo,50\nSam,38\nMerry,36\nPippin,26";
 
+function csvConverter(data) {
+    //Deteremine headers
+    let headers = data.slice(0, data.indexOf("\n")).split(",");
+    // Determine values
+    let dataValue = data.slice(data.indexof("\n") + 1).split("\n")
 
-const data = csvData.split("\n")
-const dataHeadings = data[0].split(",") // ["name", "age"]
-const dataBody = data.slice(1,5)// ["Frodo, 50 \n Sam, 38 \n Merry, 36 \n, Pippin, 26"]
-
-console.log(dataHeadings)
-for (let n = 0; n < dataBody.length; n++) {
-    dataBody[n].split(",")
-    
+    const customArr = dataValue.map(row => {
+        let values = row.split(,)
+        let obj = {}
+        values.forEach((col, idx) => {
+            if (idx < headers.length) {
+                obj[header[idx]] = col;
+            } else {
+                obj[`msi${idx}`] = col;
+            }
+        });
+        return obj;
+    })
+    return customArr
 }
-console.log(dataBody)
 
-//const sortedData = Object.fromEntries(dataBody)
-//console.log(sortedData)
-
-
-
-
-
+console.log(csvConverter(csvData))
